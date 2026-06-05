51f9618eb135ea17416730c4e4dd2fc352792ed1
antoniooreany <4531858+antoniooreany@users.noreply.github.com>
2026-06-05 17:10:01 +0200

    fix: resolve circular import by separating blueprint definition

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,9 +1,2 @@
-# from flask.blueprints import Blueprint
-
-
-from flask import Blueprint
-
-api_v1_bp: Blueprint = Blueprint(name='api_v1', import_name=__name__)
-
+from .blueprint import api_v1_bp
 from . import health
-
